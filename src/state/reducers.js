f82c363e2ee00c3da40478fb89a45672931a5acc
Jesus Jimenez <jesuswebdev@gmail.com>
2020-02-02 22:01:48 -0400

    remove category reducers

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,16 +1,14 @@
 import { combineReducers } from 'redux';
 import uiReducer from './ui/reducer';
 import userReducer from './user/reducer';
-import categoryReducer from './category/reducer';
 import questionReducer from './question/reducer';
 import suggestionReducer from './suggestion/reducer';
 
 const reducers = combineReducers({
-    ui: uiReducer,
-    user: userReducer,
-    category: categoryReducer,
-    question: questionReducer,
-    suggestion: suggestionReducer
+  ui: uiReducer,
+  user: userReducer,
+  question: questionReducer,
+  suggestion: suggestionReducer
 });
 
 export default reducers;
